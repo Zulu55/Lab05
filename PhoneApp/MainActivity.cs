b64a2b04db5d9e225c5b32b3f56fa40e35f18f3a
Zulu <jzuluaga55@gmail.com>
2017-06-04 17:18:10 -0500

    Finish

---
+++ PhoneApp/MainActivity.cs
@@ -0,0 +1,82 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using SALLab05;
+
+namespace PhoneApp
+{
+    [Activity(Label = "Phone App", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var PhoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var CallButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            CallButton.Enabled = false;
+
+            var TranslatedNumber = string.Empty;
+
+            TranslateButton.Click += (object sender, System.EventArgs e) =>
+            {
+                var Translator = new PhoneTranslator();
+                TranslatedNumber = Translator.ToNumber(PhoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(TranslatedNumber))
+                {
+                    // No hay número a llamar
+                    CallButton.Text = "Llamar";
+                    CallButton.Enabled = false;
+                }
+                else
+                {
+                    CallButton.Enabled = true;
+                }
+            };
+
+            CallButton.Click += (object sender, System.EventArgs e) =>
+            {
+                // Intentar marcar el número telefónico
+                var CallDialog = new AlertDialog.Builder(this);
+                CallDialog.SetMessage($"Llamar al número {TranslatedNumber}?");
+                CallDialog.SetNeutralButton("Llamar", delegate
+                {
+                    // Crear un intento para marcar el número telefónico
+                    var CallIntent =
+                       new Android.Content.Intent(Android.Content.Intent.ActionCall);
+                    CallIntent.SetData(
+                        Android.Net.Uri.Parse($"tel:{TranslatedNumber}"));
+                    StartActivity(CallIntent);
+                });
+                CallDialog.SetNegativeButton("Cancelar", delegate { });
+                // Mostrar el cuadro de diálogo al usuario y esperar una respuesta.
+                CallDialog.Show();
+            };
+
+            Validate();
+        }
+
+        private async void Validate()
+        {
+            var serviceClient = new ServiceClient();
+            var studentEmail = "jzuluaga55@gmail.com";
+            var password = "Roger1974";
+            var myDevice = Android.Provider.Settings.Secure.GetString(ContentResolver, Android.Provider.Settings.Secure.AndroidId);
+            var result = await serviceClient.ValidateAsync(studentEmail, password, myDevice);
+            var builder = new AlertDialog.Builder(this);
+            var alert = builder.Create();
+            alert.SetTitle("Resultado de la verificación");
+            alert.SetIcon(Resource.Drawable.Icon);
+            alert.SetMessage($"{result.Status}\n{result.Fullname}\n{result.Token}");
+            alert.SetButton("Ok", (s, ev) => { });
+            alert.Show();
+        }
+
+    }
+}
+
